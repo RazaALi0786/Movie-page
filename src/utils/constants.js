c4b3d5835b618a1e5bfe20bfda5b0abd0e6b34c3
Syed Raza Ali <razaali26296@gmail.com>
2024-05-20 03:03:06 +0530

    added s in https

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
+// export const OMDB_API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+// export const OMDB_API_URL = process.env.REACT_APP_OMDB_API_URL;
 export const OMDB_API_KEY = "5ce27a5c";
-export const OMDB_API_URL = "http://www.omdbapi.com/";
+export const OMDB_API_URL = "https://www.omdbapi.com/";
