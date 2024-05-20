0e0c2cb15c1ec864da3794ddbd2d3caa5105a2a7
Syed Raza Ali <razaali26296@gmail.com>
2024-05-20 13:38:39 +0530

    updated css

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -15,14 +15,12 @@ const MovieList = () => {
       <div className="container grid grid-4-col">
         {movie?.map((curMovie) => {
           const { imdbID, Title, Poster } = curMovie;
-          const movieName = Title.substring(0, 15);
+          const movieName = Title;
           return (
             <NavLink to={`movie/${imdbID}`} key={imdbID}>
               <div className="card">
                 <div className="card-info">
-                  <h2>
-                    {movieName.length >= 15 ? `${movieName}...` : movieName}
-                  </h2>
+                  <h2>{movieName}</h2>
                   <img src={Poster} alt="Poster" />
                 </div>
               </div>
